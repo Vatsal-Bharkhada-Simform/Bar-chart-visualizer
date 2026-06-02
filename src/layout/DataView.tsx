666bbaeb544c9dbe76cfe1742aa697e05186b577
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-02 17:05:36 +0530

    Scaffold: Add layout components

---
+++ src/layout/DataView.tsx
@@ -0,0 +1,11 @@
+import DataForm from "../components/DataForm";
+
+export default function DataView(){
+    return (
+        <>
+            <aside className="h-full p-4 flex flex-col">
+                <DataForm />
+            </aside>
+        </>
+    )
+}
