e1f026050c2d6db0ea620e6b1d387aebb70862d6
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-09 16:38:59 +0530

    Update: Destructure required attributes from input props to prevent overwriting

--- src/UI/Input.tsx
+++ src/UI/Input.tsx
@@ -2,19 +2,19 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	labelText?: string;
 }
 
-export default function Input({ labelText = "", ...props }: InputProps) {
+export default function Input({ labelText = "", id, type, ...props }: InputProps) {
 	if (labelText) {
 		return (
 			<div className="flex flex-col gap-1">
 				<label
-					htmlFor={props.id}
+					htmlFor={id}
 					className="text-base md:text-md font-inter tracking-tight text-primary"
 				>
 					{labelText}
 				</label>
 				<input
-					type={props.type}
-					id={props.id}
+					type={type}
+					id={id}
 					className="w-full px-4 py-2 bg-white border-2 border-gray-300 focus:border-blue-400 outline-none rounded-xl tracking-tight"
 					{...props}
 				/>
@@ -24,7 +24,7 @@ export default function Input({ labelText = "", ...props }: InputProps) {
 
 	return (
 		<input
-			type={props.type}
+			type={type}
 			className="w-full px-4 py-2 bg-white border-2 border-gray-300 focus:border-blue-400 outline-none rounded-xl tracking-tight"
 			{...props}
 		/>
