83ac3ed0084f79b546e085d6dd2599e47041d6f8
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-10 12:00:10 +0530

    Update: Add empty state alerts. Reset form using ref on edit mode

--- src/components/DataForm.tsx
+++ src/components/DataForm.tsx
@@ -1,11 +1,17 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import Input from "../UI/Input";
 import { ChartContext } from "../context/ChartContext";
 
 export default function DataForm() {
 	const { editData, addDataItem, updateDataItem } = useContext(ChartContext);
+    const formRef = useRef<HTMLFormElement | null>(null);
 
 	const isEditMode = editData && editData.id !== "";
+
+    useEffect(() => {
+        if(!formRef.current) return;
+        formRef.current.reset();
+    }, [editData]);
 
 	function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
 		e.preventDefault();
@@ -14,7 +20,11 @@ export default function DataForm() {
 		let label = formData.get("label");
 		const value = formData.get("value");
 
-		if (!label || !value) return;
+		if (!label || !value) {
+            alert("Please fill all the fields");
+            return;
+        }
+
 		if (typeof label !== "string" || typeof value !== "string") return;
 
 		label = label.trim();
@@ -46,6 +56,7 @@ export default function DataForm() {
 	return (
 		<form
 			onSubmit={handleSubmit}
+            ref={formRef}
 			className={`p-4 flex flex-col gap-4 border-b border-b-gray-300 ${isEditMode && "bg-orange-100"}`}
 		>
 			<Input
@@ -59,6 +70,7 @@ export default function DataForm() {
 				maxLength={50}
 				placeholder="Enter label"
 				autoFocus
+                required
 			/>
 			<Input
 				type="number"
@@ -71,10 +83,11 @@ export default function DataForm() {
 				min={0}
 				step={"any"}
 				placeholder="Enter value"
+                required
 			/>
 			<button
 				type="submit"
-				className="w-full px-6 py-2 border-none outline-none rounded-xl bg-blue-400 hover:bg-blue-500 text-white font-semibold focus:ring-3 focus:ring-blue-500"
+				className="w-full px-6 py-2 border-none outline-none rounded-xl bg-blue-500 hover:bg-blue-600 text-white font-semibold focus:ring-3 focus:ring-blue-600 cursor-pointer"
 			>
 				{isEditMode ? "Save" : "Add"}
 			</button>
